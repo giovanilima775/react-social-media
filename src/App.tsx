a10c6a134113b602b3937b00dc284fe975efd56d
Giovani Lima <pc@DESKTOP-HQRQJK8.localdomain>
2022-09-25 17:14:15 -0300

    feat: project base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"></div>
   );
 }
 
